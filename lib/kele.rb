7539a68f89bcefadad78c2377127e1c79f658546
Travis <Travisagrossman@gmail.com>
2018-04-21 11:30:31 -0700

    mentor avail 2 added endpoints to urls

--- lib/kele.rb
+++ lib/kele.rb
@@ -6,22 +6,19 @@ class Kele
   include JSON
 
   def initialize(email, password)
-    post_response = self.class.post('/sessions', body: {
-        email: email,
-        password: password
-      })
+    post_response = self.class.post(api_endpoint("sessions"), body: { "email": email, "password": password })
     @user_auth_token = post_response['auth_token']
     raise "Invalid Email or Password. Try Again." if @user_auth_token.nil?
   end
 
   def get_me
-    response = self.class.get(api_endpoint("users/me"), headers: { "authorization" => @auth_token })
+    response = self.class.get(api_endpoint("users/me"), headers: {"content_type" => 'application/json', "authorization" => @auth_token })
     JSON.parse(response.body)
   end
 
   def get_mentor_availability (mentor_id)
     id = mentor_id
-    response = self.class.get("/mentors/#{id}/student_availability", headers: { "authorization" => @auth_token })
+    response = self.class.get(api_endpoint("mentors/#{id}/student_availability"), headers: {"content_type" => 'application/json', "authorization" => @auth_token })
     mentor_avail = JSON.parse(response.body)
   end
 
