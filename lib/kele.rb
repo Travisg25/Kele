85206fd16c91dd03e1658c9f529d501bae3e1dbb
Travis <Travisagrossman@gmail.com>
2018-04-23 13:28:37 -0700

    adding get and create message methods

--- lib/kele.rb
+++ lib/kele.rb
@@ -23,6 +23,19 @@ class Kele
     mentor_avail = JSON.parse(response.body)
   end
 
+  def get_messages(page = nil)
+    if page == nil
+      response = self.class.get(api_endpoint("message_threads"), headers: { "authorization" => @auth_token})
+    else
+      response = self.class.get(api_endpoint("message_threads?page=#{page}"), headers: { "authorization" => @auth_token })
+    end
+    JSON.parse(response.body)
+  end
+
+  def create_message(recipient_id, subject, message)
+    response = self.class.post(api_endpoint("messages"), body: { "sender": self.user["email"], "recipient_id": recipient_id, "subject": subject, "stripped-text": stripped_text }, headers: {"authorization" => @auth_token })
+  end
+
   private
     def api_endpoint(endpoint)
       "https://www.bloc.io/api/v1/#{endpoint}"
